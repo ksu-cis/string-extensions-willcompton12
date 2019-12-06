8e2bf81bc541cb5a87c6d0588dc1e40e18f0c768
William Compton <will12@ksu.edu>
2019-12-06 16:11:29 -0600

    Class participation

--- StringExtensions/Program.cs
+++ StringExtensions/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using ExtentionMethods;
 
 namespace StringExtensions
 {
---
+++ StringExtensions/StringEntensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringExtensions
+{
+    public static class StringEntensions
+    {
+        public static string Capitalize(this String str)
+            {
+            string first = str[0].ToString().ToUpper();
+            return first[0] + str.Substring(1);
+            }
+        public static string ToCamelCase(this string str)
+        {
+
+            StringBuilder sb = new StringBuilder();
+            string[] parts = str.Split(" ");
+            foreach(string part in parts)
+            {
+                sb.Append(part.ToLower().Capitalize());
+            }
+            return sb.ToString();
+        }
+
+        public static int WordCount(this string s)
+        {
+          return  s.Split(new char[] { ' ', '.', '\n', '\t' }, StringSplitOptions.RemoveEmptyEntries).Length;
+        }
+    }
+}
